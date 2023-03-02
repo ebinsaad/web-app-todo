6043300a8c9aff6f67276783e95ceaac613d384d
sherif saad <ebinsaad@sherifs-MBP.cgocable.net>
2023-03-02 05:15:54 -0500

    added minimum db to the app

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# /// = relative path, //// = absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    estimate = db.Column(db.Integer)
 
 
 @app.route('/')
